2991022528a1bf47629da0a790aca37dd1c489b2
priyash manwatkar <priyashmanwatkar@gmail.com>
2026-08-26 20:30:13 +0530

    i have done some changes in the code i want to push that it to main branch

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 const Port=8080;
 const app=express();
+
+app.use(express.json())
+app.use(cors())
 app.listen(Port,(req,res)=>{
     console.log(`server is listenign ${Port}`);
 
